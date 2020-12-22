fdaaec76b92e47c37415eb1f39dfa11d7180f386
COSMOS <50519740+AmakiSora@users.noreply.github.com>
2020-12-23 00:53:19 +0800

    TS_Platform 1.03 登录功能，登录拦截器

--- src/main/java/com/cosmos/config/MyMvcConfig.java
+++ src/main/java/com/cosmos/config/MyMvcConfig.java
@@ -1,12 +1,24 @@
 package com.cosmos.config;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
+@Controller
 public class MyMvcConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("login");
         registry.addViewController("/index").setViewName("index");
-        registry.addViewController("/index.html").setViewName("index");}
+        registry.addViewController("/index.html").setViewName("index");
+        registry.addViewController("/staff.html").setViewName("staff");
+
+    }
+
+    @Override//拦截器
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginHandlerInterceptor())
+                .addPathPatterns("/**")
+                .excludePathPatterns("/login.html", "/", "/user/login", "/css/*", "js/**", "images/**", "fonts/**");
+    }
 }
