29195bba73acc931e0680a820318e3326f611493
NOCOSMOS <50519740+NOCOSMOS@users.noreply.github.com>
2021-01-28 01:24:38 +0800

    TS_Platform 1.44 作业增加说明项(使用summernote,可保存样式),

--- src/main/java/com/cosmos/pojo/Task.java
+++ src/main/java/com/cosmos/pojo/Task.java
@@ -11,6 +11,7 @@ public class Task {
     private Date issuedDate;//发布日期
     private Date deadline;//截止日期
     private String fileName;//作业文件名
+    private String introduction;//作业简介：存html格式
     //以下在数据库中不存储
     private String state;//作业状态：已截止，进行中，未开始
 }
