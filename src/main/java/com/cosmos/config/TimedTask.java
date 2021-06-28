8097e836490c2d63ac0ed83aa42f5c776406c9f8
COSMOS <50519740+AmakiSora@users.noreply.github.com>
2021-06-28 11:30:47 +0800

    TS_Platform 1.91 添加gitignore,日志记录定时任务

--- src/main/java/com/cosmos/config/TimedTask.java
+++ src/main/java/com/cosmos/config/TimedTask.java
@@ -3,6 +3,8 @@ package com.cosmos.config;
 import com.cosmos.mapper.TSMapper;
 import com.cosmos.pojo.Task;
 import com.cosmos.utils.DateUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -17,11 +19,12 @@ public class TimedTask {
     private TSMapper TSMapper;
     @Autowired
     private RedisTemplate redisTemplate;
+    private static final Logger logger = LoggerFactory.getLogger(TimedTask.class);
     @Scheduled(cron = "0 0 0 ? * *")//每日零点刷新
     public void setCurrentWeeks(){//变更周数
         DateUtils.currentWeeks = DateUtils.DateInterval(DateUtils.startDay,new Date(),"weeks");//重新计算周数(一天一算)
         DateUtils.oddEvenWeek = DateUtils.JudgmentOfWeek();//重新计算单双周
-        System.out.println("当前周数变更");
+        logger.info("当前周数变更");
     }
     @Scheduled(cron = "0 0 12 * * ?")//每天中午12点触发
     public void taskReminder(){//作业到期提醒
@@ -37,5 +40,6 @@ public class TimedTask {
                 }
             }
         }
+        logger.info("作业到期提醒");
     }
 }
